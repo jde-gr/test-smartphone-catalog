210ad715754b13231b11f29f923efeddfd2b38fb
Francisco Javier Delgado Esteban <javier.delgadoesteban@plexus.es>
2022-10-07 10:11:29 +0200

    Add conditional layout to show image and content in one column on mobile devices

--- src/components/Shop/ProductDetails.jsx
+++ src/components/Shop/ProductDetails.jsx
@@ -1,6 +1,10 @@
 import { useQuery } from 'react-query';
 import PropTypes from 'prop-types';
-import { BASE_URL, CARD_DIRECTION } from '../../core/constants';
+import {
+  BASE_URL,
+  BREAKPOINT_SIZES,
+  CARD_DIRECTION,
+} from '../../core/constants';
 import {
   ProductInfo,
   ImgContainer,
@@ -15,6 +19,7 @@ import {
 } from './styles/ProductDetails.styles';
 import Card from '../UI/Card';
 import { Price } from './styles/ProductItem.styles';
+import { useEffect, useState } from 'react';
 
 const ProductDetails = ({ id }) => {
   const fetchProduct = async () => {
@@ -44,7 +49,93 @@ const ProductDetails = ({ id }) => {
     secondaryCmera,
     dimentions,
     weight,
+    options,
   } = !!data && data;
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  const ProductContent = () => {
+    return (
+      <>
+        <ImgContainer>
+          <Image src={imgUrl} alt={`Dispositivo ${brand} ${model}`} />
+        </ImgContainer>
+        <Content>
+          <Details>
+            <Title>
+              {brand} {model}
+            </Title>
+            <Price>Precio: {price}€</Price>
+            <SubTitle>Características:</SubTitle>
+            <Desc>
+              <Key>CPU:</Key> {cpu}
+            </Desc>
+            <Desc>
+              <Key>RAM:</Key> {ram}
+            </Desc>
+            <Desc>
+              <Key>Sistema Operativo:</Key> {os}
+            </Desc>
+            <Desc>
+              <Key>Resolución de pantalla:</Key> {displayResolution}
+            </Desc>
+            <Desc>
+              <Key>Tipo de pantalla:</Key> {displayType}
+            </Desc>
+            <Desc>
+              <Key>Tamaño de pantalla:</Key> {displaySize}
+            </Desc>
+            <Desc>
+              <Key>Memoria interna:</Key> {internalMemory}
+            </Desc>
+            <Desc>
+              <Key>Memoria externa:</Key> {externalMemory}
+            </Desc>
+            <Desc>
+              <Key>Batería:</Key> {battery}
+            </Desc>
+            <Desc>
+              <Key>Cámara principal:</Key> {primaryCamera}
+            </Desc>
+            <Desc>
+              <Key>Cámara secundaria:</Key> {secondaryCmera}
+            </Desc>
+            <Desc>
+              <Key>Dimensiones:</Key> {dimentions}
+            </Desc>
+            <Desc>
+              <Key>Peso:</Key> {weight}g.
+            </Desc>
+          </Details>
+          <Actions>
+            <Desc>Actions panel</Desc>
+            <div>
+              <label for='storage'>Almacenamiento: </label>
+              <select name='storage' id='storage'>
+                {options.storages.map((storage) => (
+                  <option value={storage.code}>{storage.name}</option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label for='color'>Color: </label>
+              <select name='color' id='color'>
+                {options.colors.map((color) => (
+                  <option value={color.code}>{color.name}</option>
+                ))}
+              </select>
+            </div>
+          </Actions>
+        </Content>
+      </>
+    );
+  };
 
   return (
     <>
@@ -52,62 +143,13 @@ const ProductDetails = ({ id }) => {
       {(status === 'loading' || isFetching) && <Desc>Fetching data...</Desc>}
       {status === 'success' && !!data && (
         <ProductInfo>
-          <Card direction={CARD_DIRECTION.row} width='100%'>
-            <ImgContainer>
-              <Image src={imgUrl} alt={`Dispositivo ${brand} ${model}`} />
-            </ImgContainer>
-            <Content>
-              <Details>
-                <Title>
-                  {brand} {model}
-                </Title>
-                <Price>Precio: {price}€</Price>
-                <SubTitle>Características:</SubTitle>
-                <Desc>
-                  <Key>CPU:</Key> {cpu}
-                </Desc>
-                <Desc>
-                  <Key>RAM:</Key> {ram}
-                </Desc>
-                <Desc>
-                  <Key>Sistema Operativo:</Key> {os}
-                </Desc>
-                <Desc>
-                  <Key>Resolución de pantalla:</Key> {displayResolution}
-                </Desc>
-                <Desc>
-                  <Key>Tipo de pantalla:</Key> {displayType}
-                </Desc>
-                <Desc>
-                  <Key>Tamaño de pantalla:</Key> {displaySize}
-                </Desc>
-                <Desc>
-                  <Key>Memoria interna:</Key> {internalMemory}
-                </Desc>
-                <Desc>
-                  <Key>Memoria externa:</Key> {externalMemory}
-                </Desc>
-                <Desc>
-                  <Key>Batería:</Key> {battery}
-                </Desc>
-                <Desc>
-                  <Key>Cámara principal:</Key> {primaryCamera}
-                </Desc>
-                <Desc>
-                  <Key>Cámara secundaria:</Key> {secondaryCmera}
-                </Desc>
-                <Desc>
-                  <Key>Dimensiones:</Key> {dimentions}
-                </Desc>
-                <Desc>
-                  <Key>Peso:</Key> {weight}g.
-                </Desc>
-              </Details>
-              <Actions>
-                <Desc>Actions panel</Desc>
-              </Actions>
-            </Content>
-          </Card>
+          {width >= BREAKPOINT_SIZES.md ? (
+            <Card direction={CARD_DIRECTION.row} width='100%'>
+              {<ProductContent />}
+            </Card>
+          ) : (
+            <Card width='100%'>{<ProductContent />}</Card>
+          )}
         </ProductInfo>
       )}
     </>
--- src/core/constants.js
+++ src/core/constants.js
@@ -3,10 +3,17 @@ export const BASE_URL = 'https://front-test-api.herokuapp.com/api';
 export const HEADERHEIGHT = '5rem';
 
 export const BREAKPOINTS = {
-  sm: '@media screen and (min-width: 36rem)',
-  md: '@media screen and (min-width: 48rem)',
-  lg: '@media screen and (min-width: 62rem)',
-  xl: '@media screen and (min-width: 75rem)',
+  sm: '@media screen and (min-width: 36rem)', // 576px
+  md: '@media screen and (min-width: 48rem)', // 768px
+  lg: '@media screen and (min-width: 62rem)', // 992px
+  xl: '@media screen and (min-width: 75rem)', // 1200px
+};
+
+export const BREAKPOINT_SIZES = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
 };
 
 export const CARD_DIRECTION = {
